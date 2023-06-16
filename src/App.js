71583d472943eb526d0cbf2d04f6523a41e78641
Muhammad Abdullah <mafivs51@gmail.com>
2023-06-16 10:42:49 +0500

    basic layout/structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useState, useEffect } from "react";
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    { id: 4, title: "Womens" },
+    { id: 5, title: "Mens" },
+  ];
   return (
-    <div className="App">
-      <h1>React App</h1>
+    <div className="container-fluid">
+      <div className="row">
+        {categories.map((category) => {
+          return (
+            <div className="col-md-4 category-body-container">
+              <div></div>
+              <h3>{category.title}</h3>
+              <p>Shop Now</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
