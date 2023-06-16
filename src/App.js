e570e6949c08a7551666a67813ccfb9d5a335577
Muhammad Abdullah <mafivs51@gmail.com>
2023-06-16 12:15:52 +0500

    Routes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import { useState, useEffect } from "react";
-import "./Components/CategoryItem/categoriesItem.jsx";
-import "./Components/CategoryMenu/categoryMenu.jsx";
-import CategoryMenu from "./Components/CategoryMenu/categoryMenu.jsx";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Routes/Home/home.jsx";
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <CategoryMenu categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
